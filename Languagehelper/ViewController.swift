b837b5aa401d74194a7501455c66e55f2174f0f5
luli <meixililulu@163.com>
2016-06-17 11:47:26 +0800

    Initial Commit

---
+++ Languagehelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Languagehelper
+//
+//  Created by luli on 16/6/17.
+//  Copyright © 2016年 luli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
